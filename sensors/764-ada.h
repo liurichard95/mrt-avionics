94bd095146fadf4b9bb92f7ec6a01eeb4ae5ffae
Brendan Kellam <poptart@MacBook-Pro.local>
2016-10-26 23:37:52 -0400

    Added GPS library and created GPS data struct

--- sensors/764-ada.h
+++ sensors/764-ada.h
@@ -1,2 +1,42 @@
 
+/* 
+ Header file for Adafruit Ultimate GPS Breakout (764-ada)
+ 
+ Required Libraries:
+ 	Adafruit_GPS: github.com/adafruit/Adafruit_GPS
+
+ Description:
+ 	764-ada.ino initially establishes a Serial connection with the GPS module. Following this
+ 	connection period, the sketch is designed to remain idle until a return function is called.
+ 	This function returns a struct containing key data like latitude, longitude, altitude etc.
+*/
+
+
+/* -- guard -- */
+#ifndef gps_h
+#define gps_h
+
+/* --- includes --- */
 #include <stdio.h>
+#include <stdlib.h>
+#include <Adafruit_GPS.h>
+
+// ensures backwards compatibility with older Arduino IDEs
+#if ARDUINO >= 100
+ #include <SoftwareSerial.h>
+#endif
+
+typedef struct data {
+	char * latitude; 
+	char * longitude;
+	char * altitude;
+} GPS;
+
+/* --- function prototypes --- */
+
+GPS * allocate(void);
+
+GPS * buildGPSstruct(char * latitude, char * longitude, char * altitude);
+
+
+#endif
--- sensors/764-ada.ino
+++ sensors/764-ada.ino
@@ -1,10 +1,31 @@
 
+// include header
 #include "764-ada.h"
 
-void setup() {
-	Serial.begin(9600);
+GPS * allocate(void){
+	return (GPS *) malloc(sizeof(GPS));
 }
 
-void loop() {
-	Serial.write("hello world!\n");
+GPS * buildGPSstruct(char * latitude, char * longitude, char * altitude){
+	GPS * data = allocate();
+
+	data->latitude = latitude;
+	data->longitude = longitude;
+	data->altitude = altitude;
+
+	return data;
+}
+
+/* --- temporary setup function for testing GPS sketch individually --- */
+void setup(){
+	Serial.begin(9600);
+
+	GPS * test = buildGPSstruct("32.342234", "-52.432414", "122m");
+	Serial.write(test->latitude);
+
+}
+
+
+void loop(){
+	//Serial.write("hello world!\n");
 }
